892f0b7dcbe4ea2332d892571fa9d1a154b31657
Dmitriy <bcezbs@gmail.com>
2022-09-10 11:57:07 +0300

    Kursovik1.1

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,41 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Employee {
+    private final String employeeName;
+    private int departmentId;
+    private int employeeSalary;
+    private static int counter;
+    private final int id;
+
+    public Employee(String employeeName, int departmentId, int employeeSalary) {
+
+        this.employeeName = employeeName;
+        this.departmentId = departmentId;
+        this.employeeSalary = employeeSalary;
+        this.id = ++counter;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+    public int getDepartmentId() {
+        return departmentId;
+    }
+    public int getEmployeeSalary() {
+        return employeeSalary;
+    }
+    public int getEmployeeId() {
+        return id;
+    }
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+    public void setEmployeeSalary(int employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+    @Override
+    public String toString() {
+        return id + ": " + employeeName + "\nЗарплата: " + employeeSalary + "\nОтдел: " + departmentId;
     }
 }
+    }
+
+    }
--- src/Kursovik1.java
+++ src/Kursovik1.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Kursovik1 {
+public class Kursovik1 {
+    private static Employee[] employee = new Employee[10];
+    private static void printCredentialsEmployees() {
+        System.out.println();
+        for (Employee employee : employee) {
+            if (employee != null) {
+                System.out.println(employee.getEmployeeName());
+            }
+        }
+    }
+    private static double countAverageSalary() {
+        return countSumSalaries() / employee.length * 1.0;
+    }
+    private static Employee findMaxSalaryEmployee() {
+        int maxEmployeeSalary = -1;
+        int j = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null && employee[i].getEmployeeSalary() > maxEmployeeSalary) {
+                j = i;
+            }
+        }
+        return employee[j];
+    }
+    private static Employee findMinSalaryEmployee() {
+        int minEmployeeSalary = countSumSalaries();
+        int j = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null && employee[i].getEmployeeSalary() < minEmployeeSalary) {
+                minEmployeeSalary = employee[i].getEmployeeSalary();
+                j = i;
+            }
+        }
+        return employee[j];
+    }
+    private static void printEmployeeList() {
+        for (Employee employee : employee) {
+            if (employee != null) {
+                System.out.println(employee + "\n");
+            }
+        }
+    }
+    private static int countSumSalaries() {
+        int sumSalaries = 0;
+        for (Employee employee : employee) {
+            if (employee != null) {
+                sumSalaries += employee.getEmployeeSalary();
+            }
+        }
+        return sumSalaries;
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+        employee[0] = new Employee("Иванов Иван Иванович", 5, 50_000);
+        employee[1] = new Employee("Петров Пётр Петрович", 4, 100_000);
+        employee[2] = new Employee("Сидоров Сидр Сидорович", 3, 150_000);
+        employee[3] = new Employee("Фёдоров Фёдор Фёдорович", 2, 200_000);
+        employee[4] = new Employee("Степанов Степан Степанович", 1, 250_000);
+
+
+        printEmployeeList();
+            }
 }
